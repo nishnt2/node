b8068a4a384ba2f9cd6f0fb7425dcb96a883ec69
nishnt2 <patilnishant222@gmail.com>
2024-03-20 23:42:37 +0530

    log generation for every request

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 const http = require('http');
+const fs = require('fs');
 
 const myServer = http.createServer((req, res) => {
-  console.log('new request recieved');
-  res.end('Hello from server');
+  const log = `${Date.now()}:New Request Received\n`;
+  fs.appendFile('log.txt', log, (err, data) => {
+    res.end('Hello from server');
+  });
 });
 
 myServer.listen(8000, () => {
