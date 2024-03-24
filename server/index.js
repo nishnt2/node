64b2cc533fb5caa108760701fbd79d1e029769a3
nishnt2 <patilnishant222@gmail.com>
2024-03-25 00:07:57 +0530

    express and url packages practice

--- server/index.js
+++ server/index.js
@@ -1,12 +1,30 @@
 const http = require('http');
 const fs = require('fs');
+const url = require('url');
+const express = require('express');
 
-const myServer = http.createServer((req, res) => {
-  const log = `${Date.now()}:New Request Received\n`;
-  fs.appendFile('log.txt', log, (err, data) => {
-    res.end('Hello from server');
-  });
+//Express
+const app = express();
+app.get('/', (req, res) => {
+  return res.send('Hello From Homepage');
 });
+app.get('/about', (req, res) => {
+  return res.send(
+    'Hello From About Page' + ' ' + req.query.name + ' you are ' + req.query.age
+  );
+});
+
+const myServer = http.createServer(app);
+
+//URL Parsing
+// const myServer = http.createServer((req, res) => {
+//   const log = `${Date.now()}:New Request Received\n`;
+//   const myUrl = url.parse(req.url, true);
+//   console.log({ myUrl });
+//   fs.appendFile('log.txt', log, (err, data) => {
+//     res.end('Hello from server');
+//   });
+// });
 
 myServer.listen(8000, () => {
   console.log('server started');
